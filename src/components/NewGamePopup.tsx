d0a6d71ec38ca76066f83a22c7933a9f657eb5f0
Ivan Selchenkov <me@0xd49.xyz>
2023-05-24 13:49:30 +0200

    New game popup with Material UI

--- src/components/NewGamePopup.tsx
+++ src/components/NewGamePopup.tsx
@@ -1,3 +1,45 @@
-export default function NewGamePopup() {
-    return <div>Welcome</div>
+import {useCallback, useState} from "react"
+import {Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField} from "@mui/material"
+
+interface NewGamePopupProps {
+    open: boolean
+    onSubmit: (homeTeam: string, awayTeam: string) => void
+    onClose: () => void
+}
+
+export default function NewGamePopup(props: NewGamePopupProps) {
+    const [homeTeam, setHomeTeam] = useState('')
+    const [awayTeam, setAwayTeam] = useState('')
+
+    const handleSubmit = useCallback(() => {
+        props.onSubmit(homeTeam, awayTeam)
+    }, [homeTeam, awayTeam, props.onSubmit])
+
+    const isSubmitDisabled = homeTeam === '' || awayTeam === ''
+
+    return <Dialog open={props.open} onClose={props.onClose}>
+        <DialogTitle data-testid="game-popup-title">New Game</DialogTitle>
+        <DialogContent sx={{display: 'flex', overflow: 'visible' }} >
+            <TextField
+                onChange={(e) => setHomeTeam(e.target.value)}
+                data-testid="game-popup-home-team"
+                label="Home Team"
+                sx={{marginRight: '8px'}} />
+            <TextField
+                onChange={(e) => setAwayTeam(e.target.value)}
+                data-testid="game-popup-away-team"
+                label="Away Team" />
+        </DialogContent>
+        <DialogActions>
+            <Button data-testid="game-popup-close" onClick={props.onClose}>Close</Button>
+            <Button
+                data-testid="game-popup-submit"
+                variant="contained"
+                disabled={isSubmitDisabled}
+                onClick={handleSubmit}>
+                Submit
+            </Button>
+        </DialogActions>
+    </Dialog>
+
 }
